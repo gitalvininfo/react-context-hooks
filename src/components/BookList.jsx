6619aa7bbdc3f8f2ba3f9b185e9d979237bdf700
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-17 23:15:33 +0800

    consume context using consumer

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -9,7 +9,7 @@ class BookList extends Component {
     const theme = isLightTheme ? light : dark;
 
     return (
-      <div className="book-list" style={{ color: theme.syntax, bg: theme.bg }}>
+      <div className="book-list" style={{ color: theme.syntax, background: theme.bg }}>
         <ul>
           <li style={{ background: theme.ui }}>the way of kings</li>
           <li style={{ background: theme.ui }}>the name of the wind</li>
