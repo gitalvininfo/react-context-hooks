8fd44bc92e764f9900887d7d627ac64c8d18e331
Alvin Yanson <yansonalvin29@gmail.com>
2022-07-19 23:32:16 +0800

    intro hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
-import BookList from "./components/BookList";
-import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
-import AuthContextProvider from "./contexts/AuthContext";
-import ThemeContextProvider from "./contexts/ThemeContext";
+import SongList from "./components/SongList";
 
 function App() {
   return (
     <div className="App">
-      <AuthContextProvider>
-        <ThemeContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </ThemeContextProvider>
-      </AuthContextProvider>
+      <SongList />
     </div >
   );
 }
